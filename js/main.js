17429e6ccf439e822615e74a0743ccae66ef25e1
ekampreetkaur <113371633+ekameeraa@users.noreply.github.com>
2023-10-25 01:38:59 -0400

    design errors fixing
    updating my css and js folders

--- js/main.js
+++ js/main.js
@@ -1,54 +1,13 @@
 // Handles loading the events for <model-viewer>'s slotted progress bar
 const onProgress = (event) => {
-  const progressBar = event.target.querySelector(".progress-bar");
-  const updatingBar = event.target.querySelector(".update-bar");
-  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
-  if (event.detail.totalProgress === 1) {
-    progressBar.classList.add("hide");
-    event.target.removeEventListener("progress", onProgress);
-  } else {
-    progressBar.classList.remove("hide");
-  }
-};
-document.querySelector("model-viewer").addEventListener("progress", onProgress);
-
-// rough
-(() => {
-  //console.log("IIFE Fired");
-  //variables
-  const model = document.querySelector("#model");
-  const hotspots = document.querySelectorAll(".Hotspot");
-
-  //functions
-  function modelLoaded() {
-    //console.log(hotspots);
-    hotspots.forEach((hotspot) => {
-      hotspot.style.display = "block";
-    });
-  }
-
-  function showInfo() {
-    //console.log(this.slot);
-    //console.log(`#${this.slot}`);
-    //since the slot value matches the id value I can use the slot value as a selector to get to the div I want.
-    let selected = document.querySelector(`#${this.slot}`);
-    gsap.to(selected, 1, { autoAlpha: 1 });
-  }
-
-  function hideInfo() {
-    //console.log(this.slot);
-    //console.log(`#${this.slot}`);
-    let selected = document.querySelector(`#${this.slot}`);
-    gsap.to(selected, 1, { autoAlpha: 0 });
-  }
-
-  //Event Listener
-  model.addEventListener("load", modelLoaded);
-
-  hotspots.forEach(function (hotspot) {
-    hotspot.addEventListener("mouseover", showInfo);
-    hotspot.addEventListener("mouseout", hideInfo);
-  });
-})();
-
-// In this version, the event listeners use regular functions instead of arrow functions, so the "this" keyword inside the event listeners will refer to the DOM element that triggered the event.
+    const progressBar = event.target.querySelector('.progress-bar');
+    const updatingBar = event.target.querySelector('.update-bar');
+    updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+    if (event.detail.totalProgress === 1) {
+      progressBar.classList.add('hide');
+      event.target.removeEventListener('progress', onProgress);
+    } else {
+      progressBar.classList.remove('hide');
+    }
+  };
+  document.querySelector('model-viewer').addEventListener('progress', onProgress);
